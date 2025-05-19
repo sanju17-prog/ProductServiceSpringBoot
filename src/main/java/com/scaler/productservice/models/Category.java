09aa37d9e46a73814b762bff224b716731f2c20c
Sanjana Poptani <202012044@daiict.ac.in>
2025-05-19 23:42:10 +0530

    started db

--- src/main/java/com/scaler/productservice/models/Category.java
+++ src/main/java/com/scaler/productservice/models/Category.java
@@ -1,10 +1,12 @@
 package com.scaler.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity(name = "categories")
 public class Category extends BaseModel {
     private String title;
 }
--- src/main/java/com/scaler/productservice/models/Product.java
+++ src/main/java/com/scaler/productservice/models/Product.java
@@ -1,14 +1,18 @@
 package com.scaler.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity(name = "products")
 public class Product extends BaseModel {
     private String title;
     private double price;
     private String description;
     private String imageUrl;
+    @ManyToOne
     private Category category;
 }
